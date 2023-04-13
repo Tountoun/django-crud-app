bd6cbfb38238a10befb04ff5e41f41439a455136
Tountoun <tountounabela@gmail.com>
2023-04-13 13:49:26 +0000

    views

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('logout', views.logout_user, name='logout'),
     path('register', views.register_user, name='register'),
     path('add', views.add_employee, name='add_employee'),
+    path('detail/<int:pk>', views.employee_detail, name='emp_detail'),
+    path('delete/<int:pk>', views.delete_employee, name='delete_emp'),
+    path('update/<int:pk>', views.update_employee, name='update_emp'),
 ]
